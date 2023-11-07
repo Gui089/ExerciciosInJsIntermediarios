f4aa8889ad97ed60a752f89439fca4fa445a36e7
Gui089 <guilhermesilba811@gmail.com>
2023-11-07 09:29:06 -0300

    completed

---
+++ app.js
@@ -0,0 +1,166 @@
+/*
+  01
+
+  - Implemente um código assíncrono entre os console.log() abaixo.
+*/
+
+console.log('Linha 1');
+console.log('Linha 2');
+
+setTimeout(() => {
+  console.log('Se passrarm 3 segundos ');
+}, 3000);
+
+console.log('Linha 3');
+console.log('Linha 4');
+
+
+
+console.log('Linha 5');
+console.log('Linha 6');
+
+/* let count = 1;
+
+const interval = setInterval(() => {
+    count++;
+    console.log(count);
+}, 3000);
+
+if(count == 6) {
+  clearInterval(interval);
+} */
+
+
+console.log('Linha 7');
+console.log('Linha 8');
+
+/*
+  02
+
+  - Descomente o código abaixo e crie a função que fará a string dentro da 
+    "logGreeting" ser exibida no console.
+*/
+
+const x = (func) => {
+  func('Guilherme');
+}
+
+function logGreeting (name) {
+  console.log(`olá, ${name}`);
+}
+
+x(logGreeting);
+
+/*
+  03
+
+  - O código abaixo possui uma parte que pode ser isolada. Isole-a.
+*/
+
+const numbers = [3, 4, 10, 20];
+const getLessThanFive = num => num < 5;
+const filterNumbers = numbers.filter(getLessThanFive);
+const lesserThanFive = filterNumbers;
+
+console.log(lesserThanFive);
+
+/*
+  04
+
+  - Refatore o código abaixo.
+*/
+
+const prices = [12, 19, 7, 209];
+let totalPrice = 0;
+
+prices.forEach( price => {
+  totalPrice += price;
+});
+
+
+/* for (let i = 0; i < prices.length; i++) {
+  totalPrice += prices[i];
+} */
+
+console.log(`Preço total: ${totalPrice}`);
+
+/*
+  05
+
+  - Abaixo da declaração do objeto "car", modifique a cor do carro para 'azul';
+  - Não insira `car.color = azul`.
+*/
+
+let car = { color: 'amarelo' }
+
+let secondCar = car;
+secondCar.color = 'Azul';
+console.log(secondCar.color);
+
+
+/*
+  06
+
+  - Crie uma função que recebe 3 argumentos;
+  - Se um dos 3 argumentos não for passado na invocação, a string 'A função 
+    deve ser invocada com 3 argumentos' deve ser retornada;
+  - Se todos os argumentos forem passados, retorne a string 'A função foi 
+    invocada com 3 argumentos'.
+*/
+
+const myFun = (pram1, param2, param3) => {
+  const ifSomeParamUndefined = [pram1, param2, param3].includes(undefined);
+  if(ifSomeParamUndefined) {
+      return 'A função deve ser invocada com 3 argumentos';
+  }
+
+  return 'A função foi invocada com 3 argumentos';
+}
+
+console.log(myFun(1));
+
+/*
+  07
+
+  - O objeto abaixo representa uma caixa de livros com espaço para 5 livros e 
+    que, no momento em que foi declarado, possui nenhum livro dentro;
+  - Crie um método que irá adicionar livros na caixa;
+  
+  Este método deve:
+    - Receber por parâmetro o número de livros que serão colocados na caixa. 
+      Esse número não precisa encher a caixa de uma só vez, os livros podem 
+      ser acrescentados aos poucos;
+    - Retornar a frase: "Já há 'X' livros na caixa";
+    - Se a caixa já estiver cheia, com todos os espaços já preenchidos, o 
+      método deve retornar a frase: "A caixa já está cheia";
+    - Se ainda houverem espaços na caixa mas a quantidade de livros passada por
+      parâmetro for ultrapassar o limite de espaços da caixa, mostre quantos 
+      espaços ainda podem ser ocupados, com a frase: "Só cabem mais 
+      QUANTIDADE_DE_LIVROS_QUE_CABEM livros";
+    - Se couber somente mais um livro, mostre a palavra "livro" (no singular) 
+      na frase acima.
+*/
+
+let booksBox = {
+  spaces: 5,
+  booksIn: 0
+}
+
+booksBox.addBooks = booksQtd => {
+  if(booksBox.booksIn === booksBox.spaces) {
+    return 'Caixa esta cheia';
+  }
+
+  if(booksBox.booksIn + booksQtd > booksBox.spaces) {
+    const avalibeSpaces = booksBox.spaces - booksBox.booksIn;
+    const pluralOrSingular = avalibeSpaces === 1 ? 'Livro' : 'Livros';
+    return `Só cabem mais ${avalibeSpaces} ${pluralOrSingular}`;
+  }
+
+  booksBox.booksIn += booksQtd;
+  return `Já há ${booksBox.booksIn} livros na caixa`;
+}
+
+console.log(booksBox.addBooks(2));
+console.log(booksBox.addBooks(2));
+console.log(booksBox.addBooks(2));
